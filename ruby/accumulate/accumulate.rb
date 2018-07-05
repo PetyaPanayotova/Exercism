a9d70c64b95d4d88b8a3fae4bb1804f6d7f1ed2e
PetyaPanayotova <p.v.panayotova@gmail.com>
2018-07-05 20:06:55 +0300

    Calling #to_enum if no block is given

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -1,11 +1,15 @@
 class Array
   def accumulate(&block)
-    result = []
-    self.each { |element| result << block.call(element) }
-    result
+    if block
+      result = []
+      self.each { |element| result << block.call(element) }
+      result
+    else
+      self.to_enum(:accumulate)
+    end
   end
 end
 
 module BookKeeping
-  VERSION = 1 
+  VERSION = 1
 end
